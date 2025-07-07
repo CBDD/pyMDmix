9e9d58892ffcd862bb4bb85da01334b5d6d49ef9
ggutierrez-sb <ggutierrez@sunbright.tech>
2025-07-07 11:45:46 +0000

    add some tests

--- tests/solvent/conftest.py
+++ tests/solvent/conftest.py
@@ -1,28 +1,41 @@
-import os
 from pathlib import Path
-from typing import Any
+from typing import Any, Callable, Iterable
 
 import pytest
-from pytest import FixtureRequest
 
+from mdmix.plugins.solvent import CreateSolventRequest, Solvent, SolventInMemoryRepository, SolventRepository
 from tests.utils import load_yaml
 
+RepositoryFactory = Callable[[], SolventRepository]
+
+
+FIXTURES_PATH = Path(__file__).parent / "fixtures"
+
+CREATE_SOLVENT_REQUEST_FILENAME = FIXTURES_PATH / "solv.yml"
+
+CREATE_SOLVENT_REQUEST_DATA = load_yaml(CREATE_SOLVENT_REQUEST_FILENAME)
+
+CREATE_SOLVENT_REQUEST = CreateSolventRequest(**CREATE_SOLVENT_REQUEST_DATA)
+
+BAD_CREATE_SOLVENT_REQUEST_WRONG_FILE = CreateSolventRequest(**CREATE_SOLVENT_REQUEST_DATA)
+BAD_CREATE_SOLVENT_REQUEST_WRONG_FILE.solvents[0].off_file = Path("fakefile.off")
+
 
 @pytest.fixture
-def solv_test_path(request: FixtureRequest) -> Path:
-    return Path(os.path.dirname(request.module.__file__))
+def create_solv_config_data() -> dict[str, Any]:
+    return CREATE_SOLVENT_REQUEST_DATA
+
+
+def get_in_memory_repository(data: Iterable[Solvent] = []) -> SolventInMemoryRepository:
+    return SolventInMemoryRepository(data)
 
 
 @pytest.fixture
-def solv_fixtures_path(solv_test_path: Path) -> Path:
-    return solv_test_path / "fixtures"
+def in_memory_repository() -> SolventRepository:
+    return get_in_memory_repository()
 
 
-@pytest.fixture
-def create_solv_config_filename(solv_fixtures_path: Path) -> Path:
-    return solv_fixtures_path / "solv.yml"
-
-
-@pytest.fixture
-def create_solv_config_data(create_solv_config_filename: Path) -> dict[str, Any]:
-    return load_yaml(create_solv_config_filename)
+@pytest.fixture(params=[get_in_memory_repository])
+def repository(request: pytest.FixtureRequest) -> SolventRepository:
+    factory: RepositoryFactory = request.param
+    return factory()
--- tests/test_dummy.py
+++
@@ -1,4 +0,0 @@
-# just a dummy test
-
-def test_all_ok() -> None:
-    assert True
